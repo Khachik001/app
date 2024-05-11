48756b43e6ceb3d6012ece88d5e44c2273b330bb
khachik <xch.gevogrgyan01@gmail.com>
2024-05-11 23:34:42 +0000

    test

--- script/script.sh
+++ script/script.sh
@@ -3,7 +3,7 @@
 echo "$AWS_ECR, $APP, $AWS_REGION, $WORK_DIR"
 ls
 echo "$GITHUB_WORKSPACE"
-ls $GITHUB_WORKSPACE/$WORK_DIR
+
 
 IMAGE=$AWS_ECR/$app
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR
