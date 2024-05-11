6b0dca848891860813cf7b1c67a305d37f061c88
khachik <xch.gevogrgyan01@gmail.com>
2024-05-11 23:30:33 +0000

    test

--- script/script.sh
+++ script/script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "$AWS_ECR, $APP, $AWS_REGION, $WORK_DIR"
+ls
+echo "$GITHUB_WORKSPACE"
+ls $GITHUB_WORKSPACE/$WORK_DIR
 
-echo "$VERSION"
+IMAGE=$AWS_ECR/$app
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR
+docker build -t $IMAGE:1.0.$VERSION -f ./apps/$app/Dockerfile .
+docker push $IMAGE:1.0.$VERSION
